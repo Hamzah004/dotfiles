157f4850603bf3a960f1ee356e8c92a2ef59b723
Hamzah004 <hamzahbaniata75@gmail.com>
2025-05-14 22:50:49 +0300

    fixed some syntax problems with timer script which is using sh shell

--- .config/custom_scripts/timer.sh
+++ .config/custom_scripts/timer.sh
@@ -3,15 +3,15 @@
 CUR_TIME=$(date +%s)
 STATUS=$(cat /var/tmp/waybar_timer)
 
-if [ $STATUS == "READY" ]; then
+if [ "$STATUS" = "READY" ]; then
 	echo ""
-elif [ $STATUS == "FINISHED" ]; then
-	mpv $HOME/.config/waybar/modules/timer.wav &
+elif [ "$STATUS" = "FINISHED" ]; then
+	mpv "$HOME/.config/waybar/modules/timer.wav" &
 	echo "READY" >/var/tmp/waybar_timer
 	echo ""
-elif [[ $STATUS ]] >$CUR_TIME; then
-	DIFF=$(((STATUS - CUR_TIME)))
-	echo $(date -d @$DIFF +%M:%S)
+elif [ "$STATUS" -gt "$CUR_TIME" ] 2>/dev/null; then
+	DIFF=$((STATUS - CUR_TIME))
+	echo "$(date -d "@$DIFF" +%M:%S)"
 else
 	if [ -f "/var/tmp/waybar_timer" ]; then
 		echo "FINISHED" >/var/tmp/waybar_timer
