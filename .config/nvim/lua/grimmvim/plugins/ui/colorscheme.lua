6dead782be5a8c7ddadae9df6e8033e31f62dc06
Hamzah004 <hamzahbaniata75@gmail.com>
2025-11-20 21:51:21 +0300

    added mulicursor feature to the config of nvim

--- .config/nvim/lua/grimmvim/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/grimmvim/plugins/ui/colorscheme.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			transparent_background = true,
+			-- transparent_background = true,
 			term_colors = true,
 			integrations = {
 				treesitter = true,
---
+++ .config/nvim/lua/grimmvim/plugins/utils/multicursor.lua
@@ -0,0 +1,60 @@
+return {
+	"jake-stewart/multicursor.nvim",
+	branch = "1.0",
+	config = function()
+		local mc = require("multicursor-nvim")
+		mc.setup()
+
+		local set = vim.keymap.set
+
+		-- Add or skip adding a new cursor by matching word/selection
+		set({ "n", "x" }, "<leader>mn", function()
+			mc.matchAddCursor(1)
+		end, { desc = "Multi-cursor add next match" })
+		set({ "n", "x" }, "<leader>ms", function()
+			mc.matchSkipCursor(1)
+		end, { desc = "Multi-cursor skip next match" })
+		set({ "n", "x" }, "<leader>mN", function()
+			mc.matchAddCursor(-1)
+		end, { desc = "Multi-cursor add prev match" })
+		set({ "n", "x" }, "<leader>mS", function()
+			mc.matchSkipCursor(-1)
+		end, { desc = "Multi-cursor skip prev match" })
+
+		-- Add and remove cursors with control + left click.
+		set("n", "<c-leftmouse>", mc.handleMouse)
+		set("n", "<c-leftdrag>", mc.handleMouseDrag)
+		set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+		-- Disable and enable cursors.
+		set({ "n", "x" }, "<leader>mq", mc.toggleCursor, { desc = "Multi-cursor toggle" })
+
+		mc.addKeymapLayer(function(layerSet)
+			-- Select a different cursor as the main one.
+			layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+			layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+
+			-- Delete the main cursor.
+			layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+			-- Enable and clear cursors using escape.
+			layerSet("n", "<esc>", function()
+				if not mc.cursorsEnabled() then
+					mc.enableCursors()
+				else
+					mc.clearCursors()
+				end
+			end)
+		end)
+
+		-- Customize how cursors look.
+		local hl = vim.api.nvim_set_hl
+		hl(0, "MultiCursorCursor", { reverse = true })
+		hl(0, "MultiCursorVisual", { link = "Visual" })
+		hl(0, "MultiCursorSign", { link = "SignColumn" })
+		hl(0, "MultiCursorMatchPreview", { link = "Search" })
+		hl(0, "MultiCursorDisabledCursor", { reverse = true })
+		hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+		hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+	end,
+}
