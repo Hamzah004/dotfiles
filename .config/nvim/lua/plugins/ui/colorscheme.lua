f9ae9a844e7d077e79c8423424eb7e589e67f58e
Hamzah004 <hamzahbaniata75@gmail.com>
2026-01-29 16:03:37 +0300

    added rofi wallpaper config

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("tokyonight").setup({
-      transparent = true,
+      -- transparent = true,
       on_colors = function(colors) end,  -- Required by type definition
       on_highlights = function(highlights, colors) end,  -- Required by type definition
     })
